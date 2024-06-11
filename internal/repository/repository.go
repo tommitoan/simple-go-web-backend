d42f89cc21787a17996646adc7dba7e01896dd55
tommitoan <htm1995@gmail.com>
2024-06-11 22:15:00 +0700

    CRUD delete movie

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -17,4 +17,6 @@ type DatabaseRepo interface {
 	AllGenres() ([]*models.Genre, error)
 	InsertMovie(movie models.Movie) (int, error)
 	UpdateMovieGenres(id int, genreIDs []int) error
+	UpdateMovie(movie models.Movie) error
+	DeleteMovie(id int) error
 }
